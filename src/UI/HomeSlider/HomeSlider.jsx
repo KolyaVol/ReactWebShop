1f620c759ee0dc9bc7bbae7ef72c7c0d1c9dc9e1
VolNa <grizli887@mail.ru>
2022-11-05 18:43:51 +0300

    slider

--- src/UI/HomeSlider/HomeSlider.jsx
+++ src/UI/HomeSlider/HomeSlider.jsx
@@ -1,20 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './HomeSlider.module.css'
+import ha from './ha.jpg'
+import ah from './ah.jpg'
+
 
 export default function HomeSlider() {
+
+  let [sliderItems, setSliderItems] = useState([
+    {img: ha, id: 1},
+    {img: ah, id: 2}, 
+  ]) 
+  let [item, setItem] = useState(1)
   return (
     <div>
 
-        <div className={styles.slider__box}>
-            <div className={styles.slider}>
-                    
-            </div> 
+      <div className={styles.slider__box}>
+        <div className={styles.slider}>
+          {sliderItems.map(i=>
+            <img
+            key={i.id + 'image'}
+            src={i.img}
+            className = {item === i.id ? [styles.slider__item, styles.current].join(' ') : styles.slider__item}
+            alt='prod'
+            />
+          )}
+            <img src={ha} className={styles.slider__item1} alt='ha'/>
+            <img src={ah} className={styles.slider__item2} alt='ah'/>
+        </div> 
                
-            <div className={styles.dots}>
-                <div className={styles.dot}></div>
-                <div className={styles.dot}></div>
-            </div>
+        <div className={styles.dots}>
+          {sliderItems.map(i=>
+          <div 
+          key={i.id}
+          onClick={() => setItem(i.id)}
+          className={item === i.id ? [styles.dot, styles.current].join(' ') : styles.dot }>
+          </div>
+          )}
+          
         </div>
+      </div>
 
     </div>
   )
