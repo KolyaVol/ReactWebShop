d31f1173f4f8ffe585e8084b60b5a8425074733f
VolNa <grizli887@mail.ru>
2022-11-25 20:15:06 +0300

    user page

--- src/Pages/UserPage/UserPage.jsx
+++ src/Pages/UserPage/UserPage.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import MyButton from '../../UI/MyButton/MyButton';
 import styles from "./UserPage.module.css";
 
 export default function UserPage() {
-  return <div className={styles.UserPage}>
-    HELLO
-  </div>;
+
+
+  const userName = useSelector((state) => state.userName);
+  let firstLetter = () => {
+    if (userName) {
+      return userName.split("").slice(0, 1).join();
+    }
+    else return ''
+  } 
+
+  const dispatch = useDispatch(); 
+
+  const signOut = (event) => {
+  event.preventDefault();
+  localStorage.clear("AUTH");
+  localStorage.clear("USERNAME");
+  dispatch({ type: "USERNAME", payload: false });
+  dispatch({ type: "NOT_AUTH", payload: false});
+  console.log(userName);
+  
+};
+
+  return (
+    <div className={styles.UserPage}>
+      <div className={styles.UserBox}>
+        <div className={styles.UserTitle}>
+          <div className={styles.UserPic}>{firstLetter}</div>
+          <div className={styles.UserName}>{userName}</div>
+        </div>
+        <MyButton onClick={signOut}>Выйти</MyButton>
+      </div>
+    </div>
+  );
 }
