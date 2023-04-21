c7a2a8b6bc3b1669c48348f6d76a934e543e17c0
VolNa <grizli887@mail.ru>
2023-04-21 19:17:36 +0300

    fix bugs

--- src/hooks/usePosts.js
+++ src/hooks/usePosts.js
@@ -1,11 +1,11 @@
 import { useMemo } from "react";
 
 export const usePosts = (prods, sort, query) => {
-    
+	const sortedAndSearchedPosts = useMemo(() => {
+		return prods.filter((prod) =>
+			prod.name.toLowerCase().includes(query.toLowerCase())
+		);
+	}, [query, prods]);
 
-    const sortedAndSearchedPosts = useMemo(() => {
-        return prods.filter(prod => prod.name.toLowerCase().includes(query.toLowerCase()))
-    }, [query, prods])
-
-    return sortedAndSearchedPosts
-}
+	return sortedAndSearchedPosts;
+};
